59e7bf777d30d9b71c8223f7e1b1cc540f74fff4
mikdadayan <mikdadayan@gmail.com>
2020-06-11 12:18:39 +0400

    some minor changes in TodoList component

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,19 +2,25 @@ import React from 'react';
 import Paper from "@material-ui/core/Paper";
 import List from "@material-ui/core/List";
 import Divider from "@material-ui/core/Divider";
-import {v1 as uuidv4} from "uuid";
+import { v1 as uuidv4 } from "uuid";
 import Todo from './Todo'
-function TodoList(props){
-    if(props.todos.length){
-        return(
+function TodoList(props) {
+    if (props.todos.length) {
+        return (
             <Paper>
                 <List>
                     {props.todos.map(t => (
-                        <Todo id={t.id} task={t.task} key={uuidv4()} completed={t.completed} deleteTodo={props.deleteTodo} toggleTodo={props.toggleTodo} editTodo={props.editTodo}/>
+                        <Todo
+                            {...t}
+                            key={uuidv4()}
+                            deleteTodo={props.deleteTodo}
+                            toggleTodo={props.toggleTodo}
+                            editTodo={props.editTodo} 
+                        />
                     ))}
                 </List>
             </Paper>
-            
+
         )
     }
     return null
